12dd00a56808ee97aedd32f6438b956f88af72c5
arjun-0071 <arjun-0071@users.noreply.github.com>
2026-05-08 09:37:08 +0530

    Update socket connection to allow local network connections

--- client/src/App.js
+++ client/src/App.js
@@ -6,7 +6,7 @@ import io from 'socket.io-client';
 import {pack, unpack, generateKeys} from './services/crypt.js';
 
 const myKeys = generateKeys();
-let socket = io.connect('http://127.0.0.1:8080',{transports: ['websocket']});
+let socket = io.connect(`http://${window.location.hostname}:8080`,{transports: ['websocket']});
 export const AppContext = createContext(null);
 
 function App() {
@@ -60,7 +60,7 @@ function App() {
     if(!name || !room) return;
     setSigned(true);
     if(socket){
-      socket = io.connect('http://127.0.0.1:8080',{transports: ['websocket']});
+      socket = io.connect(`http://${window.location.hostname}:8080`,{transports: ['websocket']});
       setMessages([]);
     }
     socket.emit('join',{
